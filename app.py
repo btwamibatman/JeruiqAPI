0636891c2ec06a5ed6beb6c7b55b8946ffb96a21
zhanswontmiss <zhanserikamangazin@gmail.com>
2025-06-20 11:54:29 +0500

    some minor fixed in code structure

--- app.py
+++ app.py
@@ -1,39 +1,33 @@
 from flask import Flask, jsonify, request, render_template
-import logging
+from adapters.web.rest import api_blueprint
+from adapters.web.error_handlers import handle_exception
+from infrastructure.config import Config
+from routes.frontend import frontend_bp
+from uuid import uuid4 as uuid
 from services.ai_service.gemini_ai_model import ChatSession
 import os
-from dotenv import load_dotenv
-load_dotenv()
-
-logging.basicConfig(level=logging.DEBUG,)
 
 # Создаём экземпляр Flask
 app = Flask(__name__)
-CORS(app)
 app.config.from_object(Config)
 
-# Импортируем маршруты и обработчики ошибок
 # Загружаем конфигурацию
 from infrastructure.config import Config
 app.config.from_object(Config)
 
+# Регистрируем API
 app.register_blueprint(api_blueprint)
 app.register_blueprint(frontend_bp)
 app.register_error_handler(Exception, handle_exception)
 
-    return jsonify({'error': 'Internal Server Error'}), 500
-
-# Error handler for 400 errors
-@app.errorhandler(400)
-def bad_request(error):
-    return jsonify({'error': 'Bad Request'}), 400
+chat_sessions = {}
 
 # Добавляем маршрут для корневого URL
-@app.route("/")
 @app.route("/", methods=["GET"])
 def home():
     return jsonify({"message": "Welcome to the Jeruyiq API!"})
 
+@app.route("/response", methods=["POST"])
 def get_ai_response():
     """Handles messages from the frontend and returns AI responses."""
     data = request.get_json()
